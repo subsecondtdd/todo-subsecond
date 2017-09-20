ecad6d4557f2ae3a6ea6af2d63408c56bab32f90
Josh Chisholm <joshuachisholm@gmail.com>
2017-09-20 20:59:23 +0100

    Adding Todo to a real dom

--- test_support/DomTodoList.js
+++ test_support/DomTodoList.js
@@ -1,6 +1,18 @@
-module.exports = class DomTodoList {
-  addTodo() {
+const fs = require('fs')
+const path = require('path')
 
+module.exports = class DomTodoList {
+  constructor() {
+    const publicIndexHtmlPath = path.join(__dirname, '..', 'public', 'index.html')
+    const html = fs.readFileSync(publicIndexHtmlPath, 'utf-8')
+    this._container = document.createElement('div')
+    this._container.innerHTML = html
+    document.body.appendChild(this._container)
+  }
+
+  addTodo({ text }) {
+    this._container.querySelector('[aria-label="New Todo Text"]').value = text
+    this._container.querySelector('[aria-label="Add Todo"]').click()
   }
 
   getItems() {
