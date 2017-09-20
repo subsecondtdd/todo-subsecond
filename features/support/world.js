897ff5cad3f780c803f5891c4622b992aaf481d5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-20 22:02:43 +0100

    Make scenarios pass in both Cucumber and Cucumber-Electron

--- features/support/world.js
+++ features/support/world.js
@@ -10,17 +10,27 @@ class TodoWorld {
   constructor() {
     const todoList = new TodoList()
 
-    const publicIndexHtmlPath = path.join(__dirname, '..', '..', 'public', 'index.html')
-    const html = fs.readFileSync(publicIndexHtmlPath, 'utf-8')
-    const domNode = document.createElement('div')
-    domNode.innerHTML = html
-    document.body.appendChild(domNode)
-    mountBrowserApp({ domNode, todoList })
-
     this.contextTodoList = todoList
-    this.actionTodoList = new DomTodoList(domNode)
+
+    if (global.document) {
+      const domTodoList = createDomTodoList(todoList)
+      this.actionTodoList = domTodoList
+    } else {
+      this.actionTodoList = todoList
+    }
+
     this.outcomeTodoList = todoList
   }
+}
+
+function createDomTodoList(todoList) {
+  const publicIndexHtmlPath = path.join(__dirname, '..', '..', 'public', 'index.html')
+  const html = fs.readFileSync(publicIndexHtmlPath, 'utf-8')
+  const domNode = document.createElement('div')
+  domNode.innerHTML = html
+  document.body.appendChild(domNode)
+  mountBrowserApp({ domNode, todoList })
+  return new DomTodoList(domNode)
 }
 
 setWorldConstructor(TodoWorld)
