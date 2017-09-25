97a327625a17f228a2415a87e31909e418945417
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-25 11:18:53 +0100

    Synchronise UI

--- test_support/DomTodoList.js
+++ test_support/DomTodoList.js
@@ -4,8 +4,14 @@ module.exports = class DomTodoList {
   }
 
   async addTodo({ text }) {
-    this._domNode.querySelector('[aria-label="New Todo Text"]').value = text
-    this._domNode.querySelector('[aria-label="Add Todo"]').click()
+    return new Promise((resolve, reject) => {
+      this._domNode.addEventListener('todos:item:added', () => {
+        resolve()
+      })
+
+      this._domNode.querySelector('[aria-label="New Todo Text"]').value = text
+      this._domNode.querySelector('[aria-label="Add Todo"]').click()
+    })
   }
 
   async getItems() {
