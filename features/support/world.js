95f1520c2ef55e9f496ccd4ac6e15a139d61def8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-25 09:26:40 +0100

    Use named assemblies

--- features/support/world.js
+++ features/support/world.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const { setWorldConstructor } = require('cucumber')
+const memoize = require('map-memo')
 const TodoList = require('../../lib/TodoList')
 const HttpTodoList = require('../../lib/HttpTodoList')
 const mountBrowserApp = require('../../lib/mountBrowserApp')
@@ -16,46 +17,56 @@ if(process.env.CUCUMBER_DOM === 'true') {
   global.document = dom.window.document
 }
 
-class TodoWorld {
-  constructor() {
-    const todoList = new TodoList()
-
-    this.contextTodoList = async () => todoList
-
-    if (global.document)
-      this.actionTodoList = async () => createDomTodoList(await this.contextTodoList())
-    else if (process.env.CUCUMBER_ACTION === 'HTTP')
-      this.actionTodoList = async () => createHttpTodoList(await this.contextTodoList())
-    else
-      this.actionTodoList = this.contextTodoList
-
-    if (process.env.CUCUMBER_OUTCOME === 'DOM') {
-      this.outcomeTodoList = this.actionTodoList
-    } else
-      this.outcomeTodoList = this.contextTodoList
-  }
-}
-
-async function createHttpTodoList(todoList) {
-  const webApp = await mountWebApp({ todoList })
-  const port = 8899
-  const baseUrl = `http://localhost:${port}`
-  return new Promise((resolve, reject) => {
-    webApp.listen(port, err => {
-      if (err) return reject(err)
-      resolve(new HttpTodoList(baseUrl))
+const factory = {
+  todoList: memoize(async () => new TodoList()),
+  domTodoList: memoize(async todoList => {
+    const publicIndexHtmlPath = path.join(__dirname, '..', '..', 'public', 'index.html')
+    const html = fs.readFileSync(publicIndexHtmlPath, 'utf-8')
+    const domNode = document.createElement('div')
+    domNode.innerHTML = html
+    document.body.appendChild(domNode)
+    await mountBrowserApp({ domNode, todoList })
+    return new DomTodoList(domNode)
+  }),
+  httpTodoList: memoize(async todoList => {
+    const webApp = await mountWebApp({ todoList })
+    const port = 8899
+    const baseUrl = `http://localhost:${port}`
+    return new Promise((resolve, reject) => {
+      webApp.listen(port, err => {
+        if (err) return reject(err)
+        resolve(new HttpTodoList(baseUrl))
+      })
     })
   })
 }
 
-async function createDomTodoList(todoList) {
-  const publicIndexHtmlPath = path.join(__dirname, '..', '..', 'public', 'index.html')
-  const html = fs.readFileSync(publicIndexHtmlPath, 'utf-8')
-  const domNode = document.createElement('div')
-  domNode.innerHTML = html
-  document.body.appendChild(domNode)
-  mountBrowserApp({ domNode, todoList })
-  return new DomTodoList(domNode)
+const assemblies = {
+  'memory': {
+    contextTodoList: async () => factory.todoList(),
+    actionTodoList: async () => factory.todoList(),
+    outcomeTodoList: async () => factory.todoList(),
+  },
+  'http-domain': {
+    contextTodoList: async () => factory.todoList(),
+    actionTodoList: async () => await factory.httpTodoList(await factory.todoList()),
+    outcomeTodoList: async () => factory.todoList(),
+  },
+  'ui-domain': {
+    contextTodoList: async () => factory.todoList(),
+    actionTodoList: async () => factory.domTodoList(await factory.todoList()),
+    outcomeTodoList: async () => factory.domTodoList(await factory.todoList()),
+  },
+  'full-stack': {
+    contextTodoList: async () => factory.todoList(),
+    actionTodoList: async () => factory.domTodoList(await factory.httpTodoList(await factory.todoList())),
+    outcomeTodoList: async () => factory.domTodoList(await factory.httpTodoList(await factory.todoList())),
+  }
 }
 
+class TodoWorld {
+  constructor() {
+    Object.assign(this, assemblies[process.env.CUCUMBER_HONEYCOMB || 'memory'])
+  }
+}
 setWorldConstructor(TodoWorld)
