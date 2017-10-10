1c8fc351db64ae2f1a6b9d017302d819740123a5
Josh Chisholm <joshuachisholm@gmail.com>
2017-10-10 17:25:35 +0100

    Remove quotes from browserstack warning

--- features/run/browserstack-memory.cmd
+++ features/run/browserstack-memory.cmd
@@ -1,5 +1,5 @@
 IF "%BROWSERSTACK_USER%"=="" (
-  echo "set BROWSERSTACK_USER and BROWSERSTACK_KEY to run features on browserstack"
+  echo set BROWSERSTACK_USER and BROWSERSTACK_KEY to run features on browserstack
 ) ELSE (
   set CUCUMBER_ASSEMBLY=browserstack-memory
   node_modules\.bin\cucumber-js %*
