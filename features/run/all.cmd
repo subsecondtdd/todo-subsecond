7b37362dfc0a08508fdc2266eae689353b32ddfb
Josh Chisholm <joshuachisholm@gmail.com>
2017-10-10 17:07:49 +0100

    Trap errors

--- features/run/all.cmd
+++ features/run/all.cmd
@@ -1,8 +1,16 @@
 call features\run\memory
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\dom-memory
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\http-memory
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\dom-http-memory
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\database
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\webdriver-memory
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\webdriver-database
+if %errorlevel% neq 0 exit /b %errorlevel%
 call features\run\browserstack-memory
+if %errorlevel% neq 0 exit /b %errorlevel%
